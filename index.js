c7f77b12e6632ace249762f54be6c2d326dcc63f
Timelorde <praneethchokka@gmail.com>
2024-01-21 22:05:44 +0530

    Creation of events done. Duplicate events need to be avoided

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
-import { Client, GatewayIntentBits } from "discord.js";
 import fetch from "node-fetch";
 import config from "./config.json" with { type: "json" };
+import {
+  Client,
+  GatewayIntentBits,
+  GuildScheduledEventManager,
+  GuildScheduledEventPrivacyLevel,
+  GuildScheduledEventEntityType,
+} from "discord.js";
 
 const response = await fetch("https://codeforces.com/api/contest.list");
 const responseBody = await response.json();
 
 console.log("Response: " + responseBody["status"]);
 
-var parsedResponse = "";
+var parsedResponse = [];
 
 for (var i = 0; i < responseBody["result"].length; i++) {
   var contest = responseBody["result"][i];
   if (contest["phase"] !== "FINISHED") {
-    parsedResponse += JSON.stringify(contest);
+    parsedResponse.push(contest);
   } else {
     break;
   }
@@ -29,12 +35,14 @@ const client = new Client({
 
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log(client.channels);
+  console.log(client);
 });
 
 client.on("messageCreate", (message) => {
   if (message.content === "ping") {
     message.channel.send("pong");
+    console.log("pong");
+    console.log(message);
   }
 });
 
@@ -71,15 +79,50 @@ client.on("messageCreate", (message) => {
 client.on("messageCreate", (message) => {
   if (message.content === "fetchData") {
     // message.reply("Fk you buddy. That shit isn't working yet.");
-    message.reply(parsedResponse);
+    message.reply("test");
+    console.log(parsedResponse);
+    // message.reply(parsedResponse);
     //message.reply(apiResponse);
   }
 });
 
-client.on("messageCreate", (message) => {
+client.on("messageCreate", async (message) => {
   if (message.content === "updateEvents") {
     message.reply("That shit ain't working yet bitch");
+    const guild = client.guilds.cache.get(message.guildId);
+
+    // await eventCreator.create({
+    //   name: "Test Event",
+    //   scheduledStartTime: new Date(1787284000000),
+    //   scheduledEndTime: new Date(1787285000000),
+    //   privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
+    //   entityType: 3,
+    //   description: "This is a test Scheduled Event",
+    //   entityMetadata: { location: "www.codeforces.com" },
+    //   channel: message.channelId,
+    //   image: null,
+    //   reason: "Testing with creating a Scheduled Event",
+    // });
+
+    for (var i = 0; i < parsedResponse.length; i++) {
+      const eventCreator = new GuildScheduledEventManager(guild);
+      var contest = parsedResponse[i];
+      await eventCreator.create({
+        name: contest.name,
+        scheduledStartTime: new Date(contest.startTimeSeconds * 1000),
+        scheduledEndTime: new Date(contest.startTimeSeconds * 1000 + 7200000),
+        privacyLevel: GuildScheduledEventPrivacyLevel.GuildOnly,
+        entityType: 3,
+        description: "This is a test Scheduled Event",
+        entityMetadata: { location: "www.codeforces.com/contests" },
+        channel: message.channelId,
+        image: null,
+        reason:
+          "This is a codeforces event created by the codeforces bot as per data from the codeforces api.",
+      });
+    }
   }
+  //const eventCreator = GuildScheduledEventManager()
 });
 
 client.login(config.TOKEN);
