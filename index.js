b7baa247560ecff824e9e8ab323c2b2f9a65c14d
Timelorde <praneethchokka@gmail.com>
2024-01-21 19:03:53 +0530

    Parsing the codeforces api data

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-const { Client, GatewayIntentBits } = require("discord.js");
-var config = require("./config.json");
+import { Client, GatewayIntentBits } from "discord.js";
+import fetch from "node-fetch";
+import config from "./config.json" with { type: "json" };
+
+const response = await fetch("https://codeforces.com/api/contest.list");
+const responseBody = await response.json();
+
+console.log("Response: " + responseBody["status"]);
+
+var parsedResponse = "";
+
+for (var i = 0; i < responseBody["result"].length; i++) {
+  var contest = responseBody["result"][i];
+  if (contest["phase"] !== "FINISHED") {
+    parsedResponse += JSON.stringify(contest);
+  } else {
+    break;
+  }
+}
+
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
@@ -17,6 +35,50 @@ client.on("ready", () => {
 client.on("messageCreate", (message) => {
   if (message.content === "ping") {
     message.channel.send("pong");
+  }
+});
+
+// var options = {
+//   host: "codeforces.com",
+//   port: 443,
+//   path: "/api/contest.list",
+//   method: "GET",
+// };
+//
+// var apiResponse;
+// var req = https.request(options, (res) => {
+//   console.log("STATUS: " + res.statusCode);
+//   console.log("HEADERS: " + JSON.stringify(res.headers));
+//   res.setEncoding("utf8");
+//   res.on("data", (chunk) => {
+//     var ResponseBody = chunk;
+//     console.log("BODY: " + ResponseBody.status);
+//   });
+// });
+//
+// req.on("error", function (e) {
+//   console.log("problem with request: " + e.message);
+// });
+//
+// // write data to request body
+// req.write("data\n");
+// req.write("data\n");
+// req.end();
+//
+// console.log(req);
+//
+
+client.on("messageCreate", (message) => {
+  if (message.content === "fetchData") {
+    // message.reply("Fk you buddy. That shit isn't working yet.");
+    message.reply(parsedResponse);
+    //message.reply(apiResponse);
+  }
+});
+
+client.on("messageCreate", (message) => {
+  if (message.content === "updateEvents") {
+    message.reply("That shit ain't working yet bitch");
   }
 });
 
